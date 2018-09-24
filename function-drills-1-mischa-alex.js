ab99e82a9517918f427b355a63bf07a89fbd537a
talexcrowell <talexcrowell@gmail.com>
2018-09-24 11:27:51 -0700

    added getYearOfBirth function

--- function-drills-1-mischa-alex.js
+++ function-drills-1-mischa-alex.js
@@ -1,6 +1,10 @@
+function getYearOfBirth (age) {
+  return 2018 - age;
+}
+
 function createGreeting (name, age) {
-  const yearOfBirth = 2018-age;
-  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
+  const yob = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`;
 }
 
 const greeting1 = createGreeting();
